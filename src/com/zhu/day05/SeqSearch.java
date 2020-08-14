50092058a053c74f5bf5ed541d11c25b196c6c2b
zhupeimeng <577855901@qq.com>
2020-08-14 10:36:30 +0800

    二分查找返回集合

--- src/com/zhu/day05/SeqSearch.java
+++ src/com/zhu/day05/SeqSearch.java
@@ -1,15 +1,19 @@
 package com.zhu.day05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author zpm
  * @version 1.0
  */
 public class SeqSearch {
 
-    static int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static int[] arr = {5, 5, 5, 5, 5, 5, 5, 5};
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println(binarySearch(arr, 6, 0, arr.length - 1));
+        System.out.println(binarySearch2(arr, 5, 0, arr.length-1));
     }
 
     public static int seqFind(int[] arr, int destination) {
@@ -32,6 +36,37 @@ public class SeqSearch {
             return binarySearch(arr, destination, left, mid - 1);
         } else {
             return binarySearch(arr, destination, mid + 1, right);
+        }
+    }
+
+    public static List<Integer> binarySearch2(int[] arr, int destination, int left, int right) {
+        if (left > right || arr == null || arr.length == 0) {
+            return new ArrayList<>();
+        }
+        int mid = (left + right) / 2;
+        if (arr[mid] == destination) {
+            int temp = mid - 1;
+            //向左边扫描
+            while (true) {
+                if (temp < 0 || arr[temp] != destination) {
+                    break;
+                }
+                list.add(temp--);
+            }
+            list.add(mid);
+            //向右边扫描
+            temp = mid + 1;
+            while (true) {
+                if (temp > right || arr[temp] != destination) {
+                    break;
+                }
+                list.add(temp++);
+            }
+            return list;
+        } else if (arr[mid] > destination) {
+            return binarySearch2(arr, destination, left, mid - 1);
+        } else {
+            return binarySearch2(arr, destination, mid + 1, right);
         }
     }
 }
