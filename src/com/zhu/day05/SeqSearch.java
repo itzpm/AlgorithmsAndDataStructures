af81342baf42f05a272a46f4490ef57ad3c8d3e2
zhupeimeng <577855901@qq.com>
2020-08-14 16:47:28 +0800

    斐波拉契查找

--- src/com/zhu/day05/SeqSearch.java
+++ src/com/zhu/day05/SeqSearch.java
@@ -12,9 +12,6 @@ public class SeqSearch {
     static int[] arr = {5, 5, 5, 5, 5, 5, 5, 5};
     static List<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println(binarySearch2(arr, 5, 0, arr.length - 1));
-    }
 
     public static int seqFind(int[] arr, int destination) {
         for (int i = 0; i < arr.length; i++) {
@@ -23,51 +20,5 @@ public class SeqSearch {
             }
         }
         return -1;
-    }
-
-    public static int binarySearch(int[] arr, int destination, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
-        int mid = (left + right) / 2;
-        if (arr[mid] == destination) {
-            return mid;
-        } else if (arr[mid] > destination) {
-            return binarySearch(arr, destination, left, mid - 1);
-        } else {
-
-            return binarySearch(arr, destination, mid + 1, right);
-        }
-    }
-
-    public static List<Integer> binarySearch2(int[] arr, int destination, int left, int right) {
-        if (left > right || arr == null || arr.length == 0) {
-            return new ArrayList<>();
-        }
-        int mid = (left + right) / 2;
-        if (arr[mid] == destination) {
-            int temp = mid - 1;
-            //向左边扫描
-            while (true) {
-                if (temp < 0 || arr[temp] != destination) {
-                    break;
-                }
-                list.add(temp--);
-            }
-            list.add(mid);
-            //向右边扫描
-            temp = mid + 1;
-            while (true) {
-                if (temp > right || arr[temp] != destination) {
-                    break;
-                }
-                list.add(temp++);
-            }
-            return list;
-        } else if (arr[mid] > destination) {
-            return binarySearch2(arr, destination, left, mid - 1);
-        } else {
-            return binarySearch2(arr, destination, mid + 1, right);
-        }
     }
 }
